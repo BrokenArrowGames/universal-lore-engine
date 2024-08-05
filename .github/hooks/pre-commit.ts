fe9e6f6f5cdc21d170abf073801cbe005e8312ad
French, Caleb <caleb.m.french@gmail.com>
2024-08-05 04:13:28 +0000

    speed up pre-commit hook

---
+++ .github/hooks/pre-commit.ts
@@ -0,0 +1,44 @@
+import { extname } from "path";
+import { Runner } from "./runner";
+
+(async () => {
+  const argv = process.argv;
+  argv.shift();
+  argv.shift();
+  const srcFiles = argv.filter((file) =>
+    [".js", ".jsx", ".ts", ".tsx"].includes(extname(file)),
+  );
+  try {
+    const runner = new Runner({
+      compiling: {
+        command: "pnpm build",
+        // options: { stdio: 'inherit' },
+      },
+      "unit tests": {
+        command: "pnpm test:cov",
+        // options: { stdio: 'inherit' },
+      },
+      "integration tests": {
+        command: "pnpm test:int",
+        // options: { stdio: 'inherit' },
+      },
+      linting: {
+        command: `npx eslint ${srcFiles.join(" ")}`,
+        dependencies: ["compiling"],
+        // options: { stdio: "inherit" },
+      },
+      formatting: {
+        command: `npx prettier ${srcFiles.join(" ")}`,
+        dependencies: ["linting"],
+        // options: { stdio: "inherit" },
+      },
+    });
+    await runner.promise;
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(err.message);
+    } else {
+      console.error(err);
+    }
+  }
+})();
